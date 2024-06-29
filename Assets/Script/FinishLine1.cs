f9a1a62c6045e4d436b513781b74d3d4483649af
Ryudpis22 <115725065+Rangga2205@users.noreply.github.com>
2024-06-29 20:45:43 +0700

    Initial commit

---
+++ Assets/Script/FinishLine1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine1 : MonoBehaviour
+{
+    public int nextSceneLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 3)  
+            {
+                Debug.Log("You Completed ALL Levels");
+                
+                //Show Win Screen or Somethin.
+            }
+            else
+            {
+                //Move to next level
+                SceneManager.LoadScene(nextSceneLoad);
+
+                //Setting Int for Index
+                if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+                {
+                    PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+                }
+            }
+        }
+    }
+
+}
+
